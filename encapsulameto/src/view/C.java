3586c77180cf1f3c1a8eb1a169765bfa1a4a97f8
Lucca Barcelos <161386822+CelosLucca@users.noreply.github.com>
2025-05-08 11:33:27 -0300

    Add files via upload

---
+++ encapsulameto/src/view/C.java
@@ -0,0 +1,24 @@
+package view;
+
+import control.A;
+import model.B;
+
+public class C {
+
+    String ca1;
+    private String ca2;
+    public String ca3;
+    protected String ca4;
+
+    public void testeA() {
+        A a = new A();
+        //a.
+    }
+
+    public void testeB() {
+        B b = new B();
+        //b.
+    }
+
+}
+
