4af64c9cdcc34b980d662f0270b343385c6a2fec
Lucca Barcelos <161386822+CelosLucca@users.noreply.github.com>
2025-05-15 11:12:42 -0300

    Add files via upload

---
+++ polimorfismo/src/sobrecarga/Calculadora.java
@@ -0,0 +1,25 @@
+package sobrecarga;
+
+public class Calculadora {
+
+    // Metodo para somar dois inteiros
+    public int somar(int a, int b) {
+        return a + b;
+    }
+
+    public int somar(double x, int y) {
+        return x + y;
+    }
+
+    // Metodo para somar três inteiros
+    public int somar(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    // Metodo para somar dois números decimais
+    public double somar(double a, double b) {
+        return a + b;
+    }
+}
+
+
